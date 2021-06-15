8204e6bce78fbd4727617cc7cc8ecf71e6c74235
Sean Amos <10598927+seanamos@users.noreply.github.com>
2021-06-16 01:07:36 +0200

    Add XML docs to Stage.fs

--- FsMake/Stage.fs
+++ FsMake/Stage.fs
@@ -2,11 +2,36 @@ namespace FsMake
 
 open System
 
+/// <summary>
+/// Stages used in a pipeline.
+/// </summary>
 type Stage =
+    /// <summary>
+    /// A sequential stage the run a single step.
+    /// </summary>
+    /// <param name="step">The step to run.</param>
     | SequentialStage of step: Step
+    /// <summary>
+    /// A stage that runs all steps in parallel.
+    /// </summary>
+    /// <param name="steps">The steps to run.</param>
     | ParallelStage of steps: Step list
+    /// <summary>
+    /// A sequential stage that runs a single step if the condition is <c>true</c>.
+    /// </summary>
+    /// <param name="step">The steps to run.</param>
+    /// <param name="condition">The condition that must be <c>true</c> for the step to run.</param>
     | SequentialMaybeStage of step: Step * condition: bool
+    /// <summary>
+    /// A parallel stage that runs all steps if the condition is <c>true</c>.
+    /// </summary>
+    /// <param name="steps">The steps to run.</param>
+    /// <param name="condition">The condition that must be <c>true</c> for the step to run.</param>
     | ParallelMaybeStage of steps: Step list * condition: bool
+    /// <summary>
+    /// A parallel stage that runs steps if their individual conditions are <c>true</c>.
+    /// </summary>
+    /// <param name="steps">The steps to run.</param>
     | ParallelMaybesStage of steps: ParallelMaybe list
 
 module internal Stage =
